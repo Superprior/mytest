dc51fe1ced6957da454ec898c54ea5acbdc9f6b3
Superprior <super.prior00@gmail.com>
2023-06-05 11:07:09 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,59 +1,45 @@
-import React, { useEffect, Component } from 'react';
-import ContextState from './context_state_config';
-import history from './utils/history';
-import ReactGA from 'react-ga';
-import './App.css'
-import ttiPolyfill from 'tti-polyfill';
+import React, { useEffect, Component } from "react";
+import ContextState from "./context_state_config";
+import history from "./utils/history";
+import ReactGA from "react-ga";
+import "./App.css";
+import ttiPolyfill from "tti-polyfill";
 
-
-ReactGA.initialize('UA-144556525-1');
-
+ReactGA.initialize("UA-144556525-1");
 
 ttiPolyfill.getFirstConsistentlyInteractive().then((tti) => {
   ReactGA.timing({
     category: "Load Performace",
-    variable: 'Time to Interactive',
-    value: tti
-  })
+    variable: "Time to Interactive",
+    value: tti,
+  });
 });
 
-
-const callback = list => {
-    list.getEntries().forEach(entry => {
-      console.log(entry)
-        ReactGA.timing({
-          category: "First Meaningful Paint",
-          variable: entry.name,
-          value: entry.responseEnd
-        })
-  })
-}
+const callback = (list) => {
+  list.getEntries().forEach((entry) => {
+    console.log(entry);
+    ReactGA.timing({
+      category: "First Meaningful Paint",
+      variable: entry.name,
+      value: entry.responseEnd,
+    });
+  });
+};
 
 var observer = new PerformanceObserver(callback);
-observer.observe({entryTypes: [
-                                  // 'navigation',
-                                  // 'paint',
-                                  // 'resource',
-                                  // 'mark',
-                                  // 'measure',
-                                  // 'frame',
-                                  // 'longtask'
-                                ]})
+observer.observe({ entryTypes: ["navigation"] });
 
+history.listen((location) => {
+  if (location.pathname.includes("/user")) {
+    let rootURL = location.pathname.split("/")[1];
+    let userPage = location.pathname.split("/")[3];
 
-
-
- history.listen((location) => {
-   if(location.pathname.includes('/user')) {
-     let rootURL = location.pathname.split('/')[1]
-     let userPage = location.pathname.split('/')[3]
-
-     let pageHit = `/${rootURL}/${userPage}`
-     ReactGA.pageview(pageHit)
-   } else {
-     ReactGA.set({ page: location.pathname });
-     ReactGA.pageview(location.pathname)
-   }
+    let pageHit = `/${rootURL}/${userPage}`;
+    ReactGA.pageview(pageHit);
+  } else {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  }
 });
 
 // history.listen((location) => {
@@ -63,13 +49,12 @@ observer.observe({entryTypes: [
 // );
 
 const App = () => {
-
-    return(
-      <div className="App">
-        React
-        <ContextState />
-      </div>
-    )
-}
+  return (
+    <div className="App">
+      React
+      <ContextState />
+    </div>
+  );
+};
 
 export default App;
